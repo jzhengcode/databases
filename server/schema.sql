761d5e83b712c8b5ed9a9e8ac22239bdb1fd4887
Janet Zheng <janet.zhengg@gmail.com>
2020-06-04 21:20:37 -0700

    schema fixed

--- server/schema.sql
+++ server/schema.sql
@@ -1,24 +1,8 @@
+DROP DATABASE IF EXISTS chat;
 CREATE DATABASE chat;
 
 USE chat;
 
-DROP TABLE IF EXISTS messages;
-CREATE TABLE messages (
-  /*
-  user_id column - foreign key of users.id
-  chat_id (text) - not null
-  chat_text (text, size: 255, default: null)
-  time (type: timestamp, not null)
-  room_id - foriegn key of rooms.id
-  */
-  user_id INT,
-  FOREIGN KEY (user_id) REFERENCES users(id),
-  chat_id VARCHAR,
-  chat_text VARCHAR(1000) NULL,
-  msg_time VARCHAR,
-  room_id INT,
-  FOREIGN KEY (room_id) REFERENCES rooms(room_id)
-);
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
@@ -52,6 +36,24 @@ CREATE TABLE rooms (
   room_id INT AUTO_INCREMENT,
   PRIMARY KEY (room_id),
   room_name VARCHAR(20)
+);
+
+DROP TABLE IF EXISTS messages;
+CREATE TABLE messages (
+  /*
+  user_id column - foreign key of users.id
+  chat_id (text) - not null
+  chat_text (text, size: 255, default: null)
+  time (type: timestamp, not null)
+  room_id - foriegn key of rooms.id
+  */
+  user_id INT,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  chat_id VARCHAR(20),
+  chat_text VARCHAR(1000) NULL,
+  msg_time VARCHAR(20),
+  room_id INT,
+  FOREIGN KEY (room_id) REFERENCES rooms(room_id)
 );
 
 /*  Execute this file from the command line by typing:
